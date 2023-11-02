32933364b03e43472d83464bb6fe89f798e7833c
coderILKIN <tu758403c@code.edu.az>
2023-11-02 18:52:35 +0400

    array new task

---
+++ Arrayin icerisindeki tek ededlerin ededi ortasini tap/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Arrayin_icerisindeki_tek_ededlerin_ededi_ortasini_tap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 3, 4, 6, 7, 8, 9 ,10, 11};
+
+            int count = 0;
+
+            int total = 0;
+
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 1)
+                {
+                    total += numbers[i];
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Arrayde icerisindeki tek ededlerin ededi ortasi" + " : " + total / count);
+        }
+    }
+}
